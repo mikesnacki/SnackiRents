22677322cac49635908a398331c5c7cafd6299dd
mikesnacki <mikesnacki@gmail.com>
2019-10-12 14:58:59 -0400

    start of setting state  for the input fields

--- PropMan/ClientApp/src/components/Admin.tsx
+++ PropMan/ClientApp/src/components/Admin.tsx
@@ -3,7 +3,23 @@ import {PropertyData} from './FetchProperty'
 
 interface AddPropertyDataState {
     loading: boolean;
-    propData: PropertyData;
+    propData: PropertyData,
+    newPropData: {
+        propertyId: 0,
+        propertyName: "",
+        propertyAddress: "",
+        propertyCity: "",
+        propertyState: "",
+        propertyZip: 0,
+        propertyStudioUnitsRented: 0,
+        propertyStudioUnits: 0,
+        propertyOneBedroomUnitsRented: 0,
+        propertyOneBedroomUnits:  0,
+        propertyCatsAllowed: "",
+        propertyDogsAllowed: "",
+        propertyDescription: "",
+        propertyImage: "",
+    }
 }
 
 interface Props {}
@@ -36,6 +52,22 @@ export class AdminForm extends Component<Props, AddPropertyDataState> {
         this.state={
             loading: true,
             propData: new PropertyData,
+            newPropData:{
+                propertyId: 0,
+                propertyName: "",
+                propertyAddress: "",
+                propertyCity: "",
+                propertyState: "",
+                propertyZip: 0,
+                propertyStudioUnitsRented: 0,
+                propertyStudioUnits: 0,
+                propertyOneBedroomUnitsRented: 0,
+                propertyOneBedroomUnits:  0,
+                propertyCatsAllowed: "",
+                propertyDogsAllowed: "",
+                propertyDescription: "",
+                propertyImage: "",
+            }
         }
         this.handleSave = this.handleSave.bind(this)
     }
@@ -43,24 +75,36 @@ export class AdminForm extends Component<Props, AddPropertyDataState> {
     componentDidMount(){
         fetch("api/Property/GetPropertiesList")
          .then(response=> response.json() as Promise<PropertyData>)
-         .then(data=> {
-             this.setState({
+         .then(data=>{
+             this.setState(prevState=>({
                  propData: data,
-                 loading: false,
-                })
+                 loading: !prevState.loading,
+             }))
          })
     }
 
-    private handleSave(event: React.SyntheticEvent){
-        event.preventDefault();
-        const data = new FormData();
+    private handleSave(e: React.SyntheticEvent){
+        e.preventDefault();
+        const data = this.state.newPropData
 
-        if (this.state.propData.propertyId){
+        if (this.state.newPropData.propertyId){
             fetch("api/PropertyData/Edit", {
                 method: "PUT",
 
             })
         }
+    }
+
+    private handleChange(e: React.ChangeEvent<HTMLInputElement>){
+
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState(prevState=>({
+            ...prevState,
+            [name]: value,
+        }))
     }
 
     public render(){
@@ -81,68 +125,70 @@ export class AdminForm extends Component<Props, AddPropertyDataState> {
             <h3 className="align-center">To add a property, enter information below</h3>
             <form >
                 <div className="flex-row container-padding no-padding-bottom">
+                     <div>
+                        <input type="hidden" name="propID" value={this.state.newPropData.propertyId}></input>
+                    </div>
                     <div className="flex-col">
                         <label>Property Name</label>
-                        <input type="text" name="propName" placeholder="Property Name"></input>
+                        <input type="text" name="propName" placeholder="Property Name" defaultValue={this.state.newPropData.propertyName} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>Address</label>
-                        <input type="text" name="propAddress" placeholder="Address"></input>
+                        <input type="text" name="propAddress" placeholder="Address" defaultValue={this.state.newPropData.propertyAddress} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>City</label>
-                        <input type="text" name="propCity" placeholder="City"></input>
+                        <input type="text" name="propCity" placeholder="City" defaultValue={this.state.newPropData.propertyCity} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>State</label>
-                        <input type="text" name="propState" placeholder="State"></input>
+                        <input type="text" name="propState" placeholder="State" defaultValue={this.state.newPropData.propertyState} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>Zip</label>
-                        <input type="text" name="propZip" placeholder="Zip Code"></input>
+                        <input type="text" name="propZip" placeholder="Zip Code" defaultValue={this.state.newPropData.propertyZip} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>Studio Units Rented</label>
-                        <input type="text" name="propStudioUnitsRented" placeholder="Studios rented"></input>
+                        <input type="text" name="propStudioUnitsRented" placeholder="Studios rented" defaultValue={this.state.newPropData.propertyStudioUnitsRented} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>Studio Units</label>
-                        <input type="text" name="propStudioUnits" placeholder="Studios"></input>
+                        <input type="text" name="propStudioUnits" placeholder="Studios" defaultValue={this.state.newPropData.propertyStudioUnits} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>One Bedroom Units Rented</label>
-                        <input type="text" name="propOneBedRoomUnitsRented" placeholder="One bedrooms rented"></input>
+                        <input type="text" name="propOneBedRoomUnitsRented" placeholder="One bedrooms rented" defaultValue={this.state.newPropData.propertyOneBedroomUnitsRented} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col">
                         <label>One Bedroom Units</label>
-                        <input type="text" name="propOneBedRoomUnits" placeholder="One bedrooms"></input>
+                        <input type="text" name="propOneBedRoomUnits" placeholder="One bedrooms" defaultValue={this.state.newPropData.propertyOneBedroomUnits} onChange={this.handleChange}></input>
                     </div>
                     <div className="flex-col fill-width">
                         <label>Cats Allowed</label>
-                        <select name="propCatsAllowed">
+                        <select name="propCatsAllowed" defaultValue={this.state.newPropData.propertyCatsAllowed} >
                             <option value="No">No</option>
                             <option value="Yes">Yes</option>
                         </select>
                     </div>
                     <div className="flex-col fill-width">
                         <label>Dogs Allowed</label>
-                        <select name="propDogsAllowed">
+                        <select name="propDogsAllowed" defaultValue={this.state.newPropData.propertyDogsAllowed}>
                             <option value="No">No</option>
                             <option value="Yes">Yes</option>
                         </select>
                     </div>
                     <div className="flex-col fill-width">
                         <label className="display-block">Image</label>
-                        <input name="propertyImage" placeholder="image URL"></input>
+                        <input name="propertyImage" placeholder="image URL" defaultValue={this.state.newPropData.propertyImage} onChange={this.handleChange}></input>
                     </div>
                 </div>
                 <div className="flex-col">
                     <label className="display-block">Property Description</label>
-                    <textarea name="propertyDescription" placeholder="Property Description"></textarea>
+                    <textarea name="propertyDescription" placeholder="Property Description" defaultValue={this.state.newPropData.propertyDescription} ></textarea>
                 </div>
                 <button type="submit" className="btn-submit">Add Property</button>
             </form>
         </div>
-    )
-    }
+    )}
 }
